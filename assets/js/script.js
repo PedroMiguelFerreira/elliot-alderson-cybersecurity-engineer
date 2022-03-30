94c6a7f169f54b4d37f2eec00a09ea9e4aa65628
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-03-30 02:49:27 +0000

    Add javascript password validation code to the final test sub-section

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,12 @@ elliotImage.addEventListener("mouseover", function(){
 elliotImage.addEventListener("mouseout", function(){
     this.src="assets/images/elliot-alderson-home-light-mode-image.PNG";
 });
+
+function isValid() {
+    let password = document.getElementById("password").value;
+    if (password == "whiterose") {
+        alert("What I'm about to tell you is top secret. A conspiracy bigger than all of us. There's a powerful group of people out there that are secretly running the world. I'm talking about the guys no-one knows about, the ones that are invisible. The top 1% of the top 1%, the guys that play God without permission. You and I are going to save the world. Welcome to fsociety.")
+    } else {
+        alert("Goodbye, friend.")
+    }
+}
