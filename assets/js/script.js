8864e3e647632d6aa0f34b29ec2cb63068424fd0
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-03-31 20:15:56 +0000

    Change home image on the html and javascript files and change the background color for the whole website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // Mouseover event for Home image
 let elliotImage = document.getElementById("elliot-image");
 elliotImage.addEventListener("mouseover", function(){
-    this.src="assets/images/mr-robot-home-light-mode-image.PNG";
+    this.src="assets/images/mr-robot-home-light-mode-image.jpg";
 });
 
 // Mouseout event for Home image
 elliotImage.addEventListener("mouseout", function(){
-    this.src="assets/images/elliot-alderson-home-light-mode-image.PNG";
+    this.src="assets/images/elliot-alderson-home-light-mode-image.jpg";
 });
 
 // Alert method for Final Test sub-section
